41dd93b54ee31371f5b87fd0d40ca184c5eb02ae
quiqueman <egonzalezcrespo@aim.com>
2018-12-05 21:25:04 +0100

    refactored

--- src/main/java/calypsox/buggy/prototype/Prototype.java
+++ src/main/java/calypsox/buggy/prototype/Prototype.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 import com.calypso.tk.core.Log;
 
@@ -15,82 +17,74 @@ import calypsox.buggy.uti.ResourceReader;
  */
 public class Prototype extends ResourceReader {
 
-	/**
-	 * Gets the prototype. The filename has to start with '/'
-	 *
-	 * @param filename
-	 *            the filename
-	 * @param params
-	 *            the params
-	 * @return the prototype
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	public String getPrototype(final String filename, final PrototypeParams params) throws IOException {
+    /**
+     * Gets the prototype. The filename has to start with '/'
+     *
+     * @param filename the filename
+     * @param params   the params
+     * @return the prototype
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public String getPrototype(final String filename, final Properties params) throws IOException {
 
-		String eol = params.get("EOL");
-		if (eol == null) {
-			eol = "";
-		}
-
-		String output = readResourceEOL(filename, eol).toString();
-
-		for (final PrototypeParam entry : params) {
-			if (entry.getValue() != null) {
-				final StringBuilder sb = new StringBuilder("@");
-				sb.append(entry.getKey());
-				sb.append('@');
-				output = output.replaceAll(sb.toString(), entry.getValue());
-			}
-		}
-
-		return output;
+	String eol = params.getProperty("EOL");
+	if (eol == null) {
+	    eol = "";
 	}
 
-	/**
-	 * Read resource.
-	 *
-	 * @param filename
-	 *            the filename
-	 * @return the string builder
-	 * @throws UnsupportedEncodingException
-	 *             the unsupported encoding exception
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	public StringBuilder readResource(final String filename) throws IOException {
-		return readResourceEOL(filename, "");
+	String output = readResourceEOL(filename, eol).toString();
+
+	for (final Entry<Object, Object> entry : params.entrySet()) {
+	    if (entry.getValue() != null) {
+		final StringBuilder sb = new StringBuilder("@");
+		sb.append(entry.getKey());
+		sb.append('@');
+		output = output.replaceAll(sb.toString(), entry.getValue().toString());
+	    }
 	}
 
-	/**
-	 * Read resource EOL.
-	 *
-	 * @param filename
-	 *            the filename
-	 * @param eol
-	 *            the eol
-	 * @return the string builder
-	 * @throws UnsupportedEncodingException
-	 *             the unsupported encoding exception
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	StringBuilder readResourceEOL(final String filename, final String eol) throws IOException {
-		final InputStream is = getResourceAsStream(filename);
-		if (is == null) {
-			Log.error(this, "Can not read the template '" + filename + "'");
-			return new StringBuilder();
-		} else {
-			final BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-			final StringBuilder out = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				out.append(line);
-				out.append(eol);
-			}
-			reader.close();
-			return out;
-		}
+	return output;
+    }
+
+    /**
+     * Read resource.
+     *
+     * @param filename the filename
+     * @return the string builder
+     * @throws UnsupportedEncodingException the unsupported encoding exception
+     * @throws IOException                  Signals that an I/O exception has
+     *                                      occurred.
+     */
+    public StringBuilder readResource(final String filename) throws IOException {
+	return readResourceEOL(filename, "");
+    }
+
+    /**
+     * Read resource EOL.
+     *
+     * @param filename the filename
+     * @param eol      the eol
+     * @return the string builder
+     * @throws UnsupportedEncodingException the unsupported encoding exception
+     * @throws IOException                  Signals that an I/O exception has
+     *                                      occurred.
+     */
+    StringBuilder readResourceEOL(final String filename, final String eol) throws IOException {
+	final InputStream is = getResourceAsStream(filename);
+	if (is == null) {
+	    Log.error(this, "Can not read the template '" + filename + "'");
+	    return new StringBuilder();
+	} else {
+	    final BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+	    final StringBuilder out = new StringBuilder();
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+		out.append(line);
+		out.append(eol);
+	    }
+	    reader.close();
+	    return out;
 	}
+    }
 
 }
