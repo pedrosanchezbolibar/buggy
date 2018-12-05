b5255d19ebfa0d153569d7157c86ed67c94b1d1e
Enrique M. González <enriquemanuel.gonzalez.contractor@bbva.com>
2018-12-05 23:03:17 +0100

    small changes

--- src/main/java/calypsox/buggy/uti/ImgResource.java
+++ src/main/java/calypsox/buggy/uti/ImgResource.java
@@ -12,48 +12,46 @@ import com.calypso.tk.util.email.Base64;
  * The Class ImgResource.
  */
 public class ImgResource extends ResourceReader {
-	private static String HTML = "<img src=\"data:image/gif;base64,%s\" />";
 
-	private static String HTML_SIZE = "<img width=\"%s\" height=\"%s\" src=\"data:image/gif;base64,%s\" />";
+    /** The Constant HTML. */
+    private static final String HTML = "<img src=\"data:image/gif;base64,%s\" />";
 
-	/**
-	 * Gets the image.
-	 *
-	 * @param path
-	 *            the path
-	 * @return the image
-	 * @throws IOException
-	 */
-	public String getImage(final String resource) throws IOException {
-		return getImage(resource, null, null);
+    /** The Constant HTML_SIZE. */
+    private static final String HTML_SIZE = "<img width=\"%s\" height=\"%s\" src=\"data:image/gif;base64,%s\" />";
+
+    /**
+     * Gets the image.
+     *
+     * @param resource the resource
+     * @return the image
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public String getImage(final String resource) throws IOException {
+	return getImage(resource, null, null);
+    }
+
+    /**
+     * Gets the image.
+     *
+     * @param resource the resource
+     * @param width the width
+     * @param height the height
+     * @return the image
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public String getImage(final String resource, final String width, final String height) throws IOException {
+	final InputStream inputStream = getResourceAsStream(resource);
+
+	if (inputStream == null) {
+	    throw new FileNotFoundException("Image file not found " + resource);
+	} else {
+	    final byte[] bytes = IOUtils.toByteArray(inputStream);
+	    final String code = Base64.encodeBytes(bytes);
+	    if (width == null) {
+		return String.format(HTML, code);
+	    } else {
+		return String.format(HTML_SIZE, width, height, resource);
+	    }
 	}
-
-	/**
-	 * Gets the image.
-	 *
-	 * @param resource
-	 *            the resource
-	 * @param width
-	 *            the width
-	 * @param height
-	 *            the height
-	 * @return the image
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	public String getImage(final String resource, final String width, final String height) throws IOException {
-		final InputStream inputStream = getResourceAsStream(resource);
-
-		if (inputStream == null) {
-			throw new FileNotFoundException("[ERROR] No se ha podido cargar la imagen " + resource);
-		} else {
-			final byte[] bytes = IOUtils.toByteArray(inputStream);
-			final String code = Base64.encodeBytes(bytes);
-			if (width == null) {
-				return String.format(HTML, code);
-			} else {
-				return String.format(HTML_SIZE, width, height, resource);
-			}
-		}
-	}
+    }
 }
