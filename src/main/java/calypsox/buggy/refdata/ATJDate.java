9dc75690412ffbc48fb7583cfc4aa9fe7962d32d
quiqueman <egonzalezcrespo@aim.com>
2018-12-15 00:23:09 +0100

    javadoc

--- src/main/java/calypsox/buggy/refdata/ATJDate.java
+++ src/main/java/calypsox/buggy/refdata/ATJDate.java
@@ -9,7 +9,7 @@ import com.calypso.tk.core.JDate.DateComponents;
 import com.calypso.tk.core.JDatetime;
 
 /**
- * Access and format JDate objects
+ * Access and format JDate objects.
  */
 public class ATJDate {
 
@@ -81,62 +81,137 @@ public class ATJDate {
         return formatter.format(jdate.getDate(timeZone));
     }
 
+    /**
+     * Gets the date.
+     *
+     * @return the date
+     */
     public Date getDate() {
         return jdate.getDate();
     }
 
+    /**
+     * Gets the j datetime.
+     *
+     * @return the j datetime
+     */
     public JDatetime getJDatetime() {
         return jdate.getJDatetime();
     }
 
+    /**
+     * Gets the julian.
+     *
+     * @return the julian
+     */
     public long getJulian() {
         return jdate.getJulian();
     }
 
+    /**
+     * Gets the date fields.
+     *
+     * @return the date fields
+     */
     public DateComponents getDateFields() {
         return jdate.getDateFields();
     }
 
+    /**
+     * Gets the year.
+     *
+     * @return the year
+     */
     public int getYear() {
         return jdate.getYear();
     }
 
+    /**
+     * Gets the month.
+     *
+     * @return the month
+     */
     public int getMonth() {
         return jdate.getMonth();
     }
 
+    /**
+     * Gets the day of month.
+     *
+     * @return the day of month
+     */
     public int getDayOfMonth() {
         return jdate.getDayOfMonth();
     }
 
+    /**
+     * Gets the day of week.
+     *
+     * @return the day of week
+     */
     public int getDayOfWeek() {
         return jdate.getDayOfWeek();
     }
 
+    /**
+     * Gets the day of week as string.
+     *
+     * @return the day of week as string
+     */
     public String getDayOfWeekAsString() {
         return jdate.getDayOfWeekAsString();
     }
 
+    /**
+     * Gets the day of year.
+     *
+     * @return the day of year
+     */
     public int getDayOfYear() {
         return jdate.getDayOfYear();
     }
 
+    /**
+     * Gets the month length.
+     *
+     * @return the month length
+     */
     public int getMonthLength() {
         return jdate.getMonthLength();
     }
 
+    /**
+     * Gets the eom.
+     *
+     * @return the eom
+     */
     public ATJDate getEOM() {
         return new ATJDate(jdate.getEOM());
     }
 
+    /**
+     * Gets the julian offset.
+     *
+     * @return the julian offset
+     */
     public int getJulianOffset() {
         return jdate.getJulianOffset();
     }
 
+    /**
+     * Gets the eoq.
+     *
+     * @return the eoq
+     */
     public ATJDate getEOQ() {
         return new ATJDate(jdate.getEOQ());
     }
 
+    /**
+     * Gets the boq.
+     *
+     * @return the boq
+     */
     public ATJDate getBOQ() {
         return new ATJDate(jdate.getBOQ());
     }
