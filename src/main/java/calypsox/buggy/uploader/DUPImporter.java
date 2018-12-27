13b9e610b21e5ac061c9a8fb1cb7862e988dda7f
Enrique M. González <enriquemanuel.gonzalez.contractor@bbva.com>
2018-12-27 12:23:27 +0100

    wip

--- src/main/java/calypsox/buggy/uploader/DUPImporter.java
+++ src/main/java/calypsox/buggy/uploader/DUPImporter.java
@@ -2,6 +2,7 @@ package calypsox.buggy.uploader;
 
 import java.util.Properties;
 
+import com.calypso.tk.core.Log;
 import com.calypso.tk.publish.jaxb.CalypsoAcknowledgement;
 import com.calypso.tk.util.DataUploaderUtil;
 
@@ -15,17 +16,25 @@ public class DUPImporter {
     /**
      * Insert a CDUF file using a prototype.
      *
-     * @param owner    the owner
-     * @param template the template
-     * @param params   the params
+     * @param owner
+     *            the owner
+     * @param template
+     *            the template
+     * @param params
+     *            the params
      * @return the DUP ack
-     * @throws Exception the exception
+     * @throws Exception
+     *             the exception
      */
     public DUPAck insert(final Object owner, final String template, final Properties params) throws Exception {
-	final String messageText = new Prototype().getPrototype(owner, template, params);
+        if (template == null) {
+            Log.error(this, "template can not be null");
+            throw new IllegalArgumentException("Template can not be null");
+        }
+        final String messageText = new Prototype().getPrototype(owner, template, params);
 
-	final CalypsoAcknowledgement ack = DataUploaderUtil.uploadXML(messageText);
+        final CalypsoAcknowledgement ack = DataUploaderUtil.uploadXML(messageText);
 
-	return new DUPAck(ack);
+        return new DUPAck(ack);
     }
 }
